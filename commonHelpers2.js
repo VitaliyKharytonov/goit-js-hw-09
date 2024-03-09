c9bbbe3c21ed0a4af2d6ba5b9e304f3d2153dbf5
VitaliyKharytonov <113349628+VitaliyKharytonov@users.noreply.github.com>
2024-03-09 20:04:05 +0000

    Deploying to gh-pages from @ VitaliyKharytonov/goit-js-hw-09@90efc493d495360c411edba2b8fc3a8bf1d4bac1 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/styles-a468a668.js";const e=document.querySelector(".feedback-form"),a="feedback-form-state",s=JSON.parse(localStorage.getItem(a));try{e.elements.email.value=s.email,e.elements.message.value=s.message}catch{console.log("No saved data")}e.addEventListener("input",t=>{const o=e.elements.email.value.trim(),l=e.elements.message.value.trim();localStorage.setItem(a,JSON.stringify({email:o,message:l}))});e.addEventListener("submit",t=>{t.preventDefault(),localStorage.removeItem(a),e.reset()});
+//# sourceMappingURL=commonHelpers2.js.map
